52cce28e46b6ab98e7c67e2ad49f1679f1727f16
willer <130481969+unknown00707@users.noreply.github.com>
2026-06-22 12:55:50 +0900

    Add Pool Sys
    - pool sys of bullet
    - add bullet cs

--- Assets/Scripts/Runtime/Gun.cs
+++ Assets/Scripts/Runtime/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Gun : MonoBehaviour
 {
@@ -6,21 +7,72 @@ public class Gun : MonoBehaviour
     public PlayerController playerController;
     private float lastFireTime = 0f;
 
+    private IObjectPool<GameObject> bulletPool;
+    private void Awake()
+    {
+        // 오브젝트 풀 초기화 설정
+        bulletPool = new ObjectPool<GameObject>(
+            createFunc: CreateBullet,       
+            actionOnGet: OnGetBullet,       
+            actionOnRelease: OnReleaseBullet,
+            actionOnDestroy: OnDestroyBullet,
+            collectionCheck: true,
+            defaultCapacity: 20,            
+            maxSize: 50                    
+        );
+    }
     public void Fire()
     {
         if (gunData == null || gunData.bulletPrefab == null || Time.time < lastFireTime + gunData.fireRate)
         {
-            Debug.LogWarning("GunData or bulletPrefab is not assigned.");
+            Debug.LogWarning("GunData, bulletPrefab missing or FireRate cooldown.");
             return;
         }
-        // Instantiate the bullet prefab at the gun's position and rotation
-        GameObject bullet = Instantiate(gunData.bulletPrefab, transform.position, transform.rotation);
-        // Add force to the bullet to propel it forward
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.AddForce(transform.right * gunData.range, ForceMode.Impulse);
 
-        playerController?.rb.AddForce(-transform.right * gunData.recoilForce, ForceMode.Impulse);
+        GameObject bullet = bulletPool.Get();
+
+        
+        bullet.transform.position = transform.position + transform.up * 0.5f;
+        bullet.transform.rotation = transform.rotation;
+
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero; 
+            rb.angularVelocity = Vector3.zero; 
+            rb.AddForce(transform.up * gunData.range, ForceMode.Impulse);
+        }
+
+        
+        playerController?.rb.AddForce(-transform.up * gunData.recoilForce, ForceMode.Impulse);
 
         lastFireTime = Time.time;
     }
+
+    #region 오브젝트 풀 내부 함수들
+    private GameObject CreateBullet()
+    {
+        GameObject bullet = Instantiate(gunData.bulletPrefab);
+        
+        Bullet bulletScript = bullet.GetComponent<Bullet>();
+        bulletScript?.SetPool(bulletPool);
+        
+        return bullet;
+    }
+
+    private void OnGetBullet(GameObject bullet)
+    {
+        bullet.SetActive(true); 
+    }
+
+    private void OnReleaseBullet(GameObject bullet)
+    {
+        bullet.SetActive(false); 
+    }
+
+    private void OnDestroyBullet(GameObject bullet)
+    {
+        Destroy(bullet); 
+    }
+    #endregion
 }
